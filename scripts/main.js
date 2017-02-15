ae7919478f3440bde7152642727fcc54f32b21d8
Lindsay <l.e.hopper@gmail.com>
2017-02-15 14:19:32 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/editingpenandpaper.jpg') {
+      myImage.setAttribute ('src','images/openbookandlaptop.jpg');
+    } else {
+      myImage.setAttribute ('src','images/editingpenandpaper.jpg');
+    }
+}
+/* var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+} */
